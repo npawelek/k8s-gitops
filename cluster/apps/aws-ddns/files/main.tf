4a23c526e6faa9a4a980dca2ecfb2586208144d2
Nathan Pawelek <nathan@nathanpawelek.com>
2023-05-05 11:47:13 -0500

    remove unnecessary config with switch to backend
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

--- cluster/apps/aws-ddns/files/main.tf
+++ cluster/apps/aws-ddns/files/main.tf
@@ -17,21 +17,6 @@ variable "AWS_DOMAIN" {
   description = "Use the defined domain within TF_VAR_AWS_DOMAIN environment variable"
 }
 
-variable "AWS_KMS_ARN" {
-  type        = string
-  description = "Use the defined KMS ARN within TF_VAR_AWS_KMS_ARN environment variable"
-}
-
-variable "AWS_S3_BUCKET" {
-  type        = string
-  description = "Use the defined S3 bucket within TF_VAR_AWS_S3_BUCKET environment variable"
-}
-
-variable "AWS_S3_KEY" {
-  type        = string
-  description = "Use the defined S3 object key within TF_VAR_AWS_S3_KEY environment variable"
-}
-
 provider "aws" {}
 
 data "aws_route53_zone" "domain" {
