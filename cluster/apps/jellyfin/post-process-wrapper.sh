588bd33bb93b1a7986f5bf9718da623d0b2d8a65
Nathan Pawelek <nathan@nathanpawelek.com>
2025-09-21 09:10:01 -0500

    cleanup unnecessary post processing files
    Signed-off-by: Nathan Pawelek <nathan@nathanpawelek.com>

--- cluster/apps/jellyfin/post-process-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-exec > "/config/log/$(date +"%Y-%m-%d_%H-%M-%S")-post-process-wrapper-sh.log" 2>&1
-if [ -z "${1}" ]; then
-  echo "No {path} value was passed in by Jellyfin"
-  exit 1
-fi
-/usr/bin/bash /config/scripts/post-process.sh "${1}"
